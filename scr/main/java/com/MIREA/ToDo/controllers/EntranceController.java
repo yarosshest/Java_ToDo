ee62439fb9f8b153145c96241187ff4427903fc6
AndB0ndar <andrey.bondar.2003@list.ru>
2022-09-29 12:03:24 +0300

    add button

--- scr/main/java/com/MIREA/ToDo/controllers/EntranceController.java
+++ scr/main/java/com/MIREA/ToDo/controllers/EntranceController.java
@@ -15,17 +15,30 @@ import data.sql.UserSQL;
 public class EntranceController {
 
     @GetMapping("/")
-    public String Home(Model model) {
+    public String Entrance(Model model) {
         model.addAttribute("title", "Вход");
-        return "home";
+        return "Entrance/Entrance";
     }
+    @GetMapping("/Registration")
+    public String Registration(Model model) {
+        model.addAttribute("title", "Регистрация");
+        return "Entrance/Registration";
+    }
+
     @PostMapping("/")
-    private String SignIn(@RequestParam String email, @RequestParam String password, Model model) throws SQLException, ClassNotFoundException {
+    private String LogIn(@RequestParam String email, @RequestParam String password, Model model) throws SQLException, ClassNotFoundException {
         MasterSQL m = new MasterSQL();
         UserSQL a = new UserSQL(m);
         if (a.CheckUser(email, password))
             return "MainPage";
         else
-            return "home";
+            return "Entrance/Entrance";
+    }
+    @PostMapping("/Registration")
+    private String SignIn(@RequestParam String email, @RequestParam String password, @RequestParam String institute, @RequestParam String group, Model model) throws SQLException, ClassNotFoundException {
+        MasterSQL m = new MasterSQL();
+        UserSQL a = new UserSQL(m);
+        a.AddUser(email, password);
+        return "redirect:Entrance/Entrance";
     }
 }
--- scr/main/java/data/ScheduleCourse.java
+++ scr/main/java/data/ScheduleCourse.java
@@ -1,4 +1,4 @@
-package java.data;
+package data;
 
 import java.io.File;
 
